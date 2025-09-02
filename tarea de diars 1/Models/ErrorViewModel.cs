2bf3e00fe9d40489aae71391596a5826678b804a
XeanP <101090719+XeanP@users.noreply.github.com>
2025-09-02 17:46:43 -0500

    Agregar archivos de proyecto.

---
+++ tarea de diars 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tarea_de_diars_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
